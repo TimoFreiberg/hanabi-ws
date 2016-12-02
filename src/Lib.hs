b6e5cdff92522a190f8dcef2ab7761062582d54e
Timo Freiberg <timo.freiberg@novatec-gmbh.de>
2016-12-02 15:10:34 +0100

    Add serialization of basic request types

--- src/Lib.hs
+++ src/Lib.hs
@@ -3,15 +3,17 @@
 
 module Lib where
 
-import qualified Data.Text as Text
 import Data.Text (Text)
-import Data.Aeson
-import Data.ByteString.Lazy (ByteString)
+import Data.Aeson (ToJSON, FromJSON)
+import qualified Data.Aeson as Aeson
+import qualified Data.Aeson.Types as Aeson
+
 import GHC.Generics (Generic)
+import Data.List (isPrefixOf)
 
 import Hanabi.Types
 
-data Message
+data Request
   = Connect Text
   | Discard Card
   | HintColor Color
@@ -19,18 +21,52 @@ data Message
   | Play Card
   deriving (Show, Eq, Ord, Generic)
 
-instance ToJSON Message where
-  toJSON msg = object (msgType msg : payload msg)
+instance ToJSON Number
 
-payload
-  :: KeyValue t
-  => Message -> [t]
-payload (Connect name) = ["name" .= name]
+instance ToJSON Color
 
-msgType
-  :: KeyValue kv
-  => Message -> kv
-msgType msg = "type" .= getType msg
-  where
-    getType :: Message -> Text
-    getType (Connect _) = "CONNECTION_REQUEST"
+instance ToJSON Card where
+  toJSON = Aeson.genericToJSON customOptions
+  toEncoding = Aeson.genericToEncoding customOptions
+
+instance ToJSON Request where
+  toJSON = Aeson.genericToJSON customOptions
+  toEncoding = Aeson.genericToEncoding customOptions
+
+instance FromJSON Number
+
+instance FromJSON Color
+
+instance FromJSON Card where
+  parseJSON = Aeson.genericParseJSON customOptions
+
+instance FromJSON Request where
+  parseJSON = Aeson.genericParseJSON customOptions
+
+customOptions :: Aeson.Options
+customOptions =
+  Aeson.defaultOptions
+  { Aeson.fieldLabelModifier = labelMod
+  , Aeson.constructorTagModifier = typeMod
+  , Aeson.sumEncoding = typeEncoding
+  }
+
+typeEncoding :: Aeson.SumEncoding
+typeEncoding =
+  Aeson.TaggedObject
+  { Aeson.tagFieldName = "type"
+  , Aeson.contentsFieldName = "payload"
+  }
+
+labelMod :: String -> String
+labelMod s
+  | "_" `isPrefixOf` s = drop 1 s
+  | otherwise = s
+
+typeMod :: String -> String
+typeMod "Connect" = "CONNECTION_REQUEST"
+typeMod "Discard" = "DISCARD_CARD_REQUEST"
+typeMod "HintColor" = "HINT_COLOR_REQUEST"
+typeMod "HintNumber" = "HINT_NUMBER_REQUEST"
+typeMod "Play" = "PLAY_CARD_REQUEST"
+typeMod s = error ("request type " ++ s ++ "not recognized")
